ea1b54cc4e14f5e63980469aaf773431449a984a
georgequank <georgequank11@gmail.com>
2022-09-01 13:25:35 -0500

    part 2

---
+++ lab_2.py
@@ -0,0 +1,34 @@
+###dog_years problem
+#dog_year = 7 #stores the int 7 as dog_year to represent 1 human year as a dog year
+#human_age = float(input("Enter your age: ")) #this takes the human age as an float input and stores it as human_age
+#dog_age = (human_age * dog_year) #this takes human_age, multiplies it by 7, then stores it as dog_age
+#print("Your age in dog years is: " + str(dog_age)) #this converts dog_age to a string and adds it to "age in dog years", then prints it
+#----------------------------------------------------------------------------------------------------------------------------------------------
+
+###Create a script that asks the user for name, lastname, and age. Print out the info as follow:
+### Hi name lastname, you're age years old, damn
+#name = input("What is your first name: ") #takes first name input as a string, stores as name
+#last_name = input("What is your last name: ") #takes last name input as a string, stores as last_name
+#age = input("What is your age: ") #takes age as a string, stores it as age
+#print("Hi", name, last_name + ", you're", age, "years old, damn!") #prints out name, last_name, and age in a string
+#----------------------------------------------------------------------------------------------------------------------------------------------
+
+###Horse age problem
+#get inputs
+#human_age = float(input("What is your age: "))
+#calculate age
+#horse_age = (3 * ((((human_age ** 2) - 47) / 7) + 12))
+#print results
+#print("Your age in horse years is", horse_age)
+#----------------------------------------------------------------------------------------------------------------------------------------------
+
+### To calculate a humans height, use age^2.
+### Print out the humans age and estimated height
+#inputs
+#age = float(input("What is your age: "))
+#calculate
+#estimated_height = (age ** 2)
+#print
+#print("Your estimated height is", estimated_height, "centimeters")
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
