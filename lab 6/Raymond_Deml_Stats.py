49d438fb240bed11f4c0ba366a710de27b093c02
georgequank <georgequank11@gmail.com>
2022-09-29 12:46:06 -0500

    started work on lab 6'

---
+++ lab 6/Raymond_Deml_Stats.py
@@ -0,0 +1,17 @@
+#define variables and imports
+data = [1, 24, 33, 15, 26]
+import statistics
+
+#-----------------------------------------------------------------------------------
+#finding the mean
+def mean(data):
+    result = statistics.mean(data)
+    return result
+
+#-----------------------------------------------------------------------------------
+#finding the median
+def median(data):
+    result = statistics.median(data)
+    return result
+
+#-----------------------------------------------------------------------------------
