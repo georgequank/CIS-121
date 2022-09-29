e7f539658f15fa01486fdba33b6c71ef751bfae1
georgequank <georgequank11@gmail.com>
2022-09-29 13:37:31 -0500

    dumb

---
+++ lab 6/Raymond_Deml_MyProgram.py
@@ -0,0 +1,7 @@
+###opening file
+file = open("500DayFruitData.txt", 'r')
+
+#Getting all the lines and seperating them by /n
+data = file.read().splitless()
+
+print(data[1].split())
---
+++ mavpass_w6d2.py
@@ -0,0 +1,23 @@
+###
+#things to do with a word problem
+#1 what asking for
+#2 whats given
+#3 conditions
+
+#work on word problems!!!
+
+# divisor_count = 0
+
+
+# for bird in range(1, 201):
+#     for divisor in range(1, bird):
+#         if bird % divisor != 0:
+#             divisor_count += 1
+#     print("Bird", bird, "has", divisor_count, "offspring")
+# divisor_count = 0
+
+###
+#work on loops
+#restrictions
+#1 must use both loops (for and while)
+#2 loops must be able to interchange each other (counter can be added or removed for while)
