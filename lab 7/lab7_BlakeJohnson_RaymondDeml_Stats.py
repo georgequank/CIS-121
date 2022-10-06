1629929271cb7bb9b4597d3ec3edb9fe9dac72a3
georgequank <georgequank11@gmail.com>
2022-10-06 12:39:58 -0500

    did lab 7

---
+++ lab 7/lab7_BlakeJohnson_RaymondDeml_Stats.py
@@ -0,0 +1,12 @@
+# modules
+import statistics
+
+def median(data):
+    result = statistics.median(data)
+    return result
+def mean(data):
+    result = statistics.mean(data)
+    return result
+def mode(data):
+    result = statistics.mode(data)
+    return result
